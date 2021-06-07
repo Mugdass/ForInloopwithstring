cfd28a54153598352d74ae39dfc9d7838aed7d6c
Mugdass <84940755+Mugdass@users.noreply.github.com>
2021-06-07 13:25:32 -0700

    Add files via upload

---
+++ for_in_loop_with_string.py
@@ -0,0 +1,14 @@
+
+# for in loops with string
+
+
+
+
+food={"Jan":[1],"Feb":[2],"March":[3],"April":[4],"May":[5],"June":[6],"July":[7],"August":[8],"September":[9],"October":[10],"November":[11],"December":[12]}
+for f in food:
+    (print)("These are the same 12 months every year : ",f)
+    
+    
+    
+    
+
